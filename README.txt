51e525c48824e5c13a5d0e7d15d8812671716797
Suyash-Deshmukh <89959627+Suyash-Deshmukh@users.noreply.github.com>
2024-04-16 22:17:35 -0400

    updating for pythonanywhere website

--- README.txt
+++ README.txt
@@ -2,3 +2,4 @@ We are assuming the worst case scenario and continuing with just flask.
 Visibily.py is now added to see how well bootstrap and flask can render images
 
 Main: catalog.py
+wsgi.py is only there to make it easier to run the flask app, is not needed on the website.
--- catalog.py
+++ catalog.py
@@ -4,39 +4,8 @@ import scipy.stats as st
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-#get_ipython().run_line_magic('matplotlib', 'inline')
-
-#import matplotlib
-#matplotlib.use('Agg')
-
-#from astroquery.simbad import Simbad
-#from astropy.io import ascii
-#from astropy import constants as const
-#from astropy.time import Time
-#from astropy import units as u
 from astropy.coordinates import *
-#from astropy.modeling.blackbody import blackbody_lambda, blackbody_nu
-#from astropy.table import Table
-#from astroplan import (AltitudeConstraint, AirmassConstraint, AtNightConstraint)
-#from astroplan import FixedTarget, Observer
-#from astroplan import is_observable, is_always_observable, months_observable
-#from astroplan import observability_table
-#from astroplan import FixedTarget, Observer
-#from astropy.coordinates import EarthLocation
-#from astroplan.plots import plot_airmass
-#from astroplan.plots import plot_sky
-#from astropy.time import Time
-#import scipy.optimize as optimization
-#from scipy.optimize import curve_fit
-#from scipy.stats import linregress
-#from sklearn.neighbors import KernelDensity
-from subprocess import *
-#import matplotlib.image as mpimg
-
-
 import sys
-import numpy as np
-import pandas as pd
 from astroquery.simbad import Simbad
 from subprocess import *
 from flask import Flask
@@ -50,9 +19,9 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 #attempts to prevent caching
 
 
-@app.route('/test2.html') #if home if spressed, render home template
+@app.route('/home') #if home if spressed, render home template
 def home():
-    return render_template("test2.html")
+    return render_template("home.html")
 
 @app.route('/about') #"about us" page
 def about_page():
@@ -94,7 +63,7 @@ def server():
         return render_template("log.html", t1=table.to_html(index=False), t2=visib.to_html(index=False), obj_list=obj_list, len=len(obj_list))
 
     else:
-        return render_template("test2.html")
+        return render_template("home.html")
 
 if __name__ == "__main__":
     app.run()
